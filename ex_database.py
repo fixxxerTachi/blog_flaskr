e5ca6c19dbd6a85f827cb98a1245ea218fa5d84c
fixxxerTachi <fixxxer_tachi@yahoo.co.jp>
2014-03-19 12:13:30 +0900

    Update ex_database.py

--- ex_database.py
+++ ex_database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-engine=create_engine('mysql://root:pass@localhost/prac_flask')
+engine=create_engine('mysql://username:password@localhost/prac_flask')
 db_session=scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base=declarative_base()
 Base.query=db_session.query_property()
