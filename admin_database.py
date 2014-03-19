943621286009de9edb31d950544e4c38615ec281
fixxxerTachi <fixxxer_tachi@yahoo.co.jp>
2014-03-19 12:12:33 +0900

    Update admin_database.py

--- admin_database.py
+++ admin_database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-engine=create_engine('mysql+mysqldb://tachi:tachi@localhost/online_store?charset=utf8',convert_unicode=True,echo=True)
+engine=create_engine('mysql+mysqldb://username:password@localhost/online_store?charset=utf8',convert_unicode=True,echo=True)
 db_session=scoped_session(sessionmaker(bind=engine))
 Base=declarative_base()
 Base.query=db_session.query_property()
